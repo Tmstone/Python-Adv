5c123134f8279e0cc38ad4845924c6cdef4dce15
Terence Stone <terence.stone25@gmail.com>
2020-01-23 13:08:08 -0500

    Updated the resturant class to increment customers served.

--- restaurant.py
+++ restaurant.py
@@ -3,23 +3,40 @@ class Restaurant():
     def __init__(self, name, cuisine_type):
         self.name = name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
     
     def describe_restaurant(self):
         print(self.name.title() + " produces " + self.cuisine_type + ".")
 
     def open_restaurant(self):
-        print("The " + self.name.title() + " is open.\n")
+        print("The " + self.name.title() + " is open.")
 
+    def service(self):
+        ''' Print a statement showing the number of customers served '''
+        print(f'{self.name} has served {self.number_served} customers.\n')
+
+# adding method set_nummber_served
+    def set_number_served(self, number):
+        number = self.number_served
+
+# adding a method to increment_number_served
+    def increment_number_served(self, customers):
+        self.number_served += customers
 
 new_restaurant = Restaurant('Dat Jerk', 'Caribbean Food')
 new_restaurant.describe_restaurant()
 new_restaurant.open_restaurant()
+new_restaurant.increment_number_served(30)
+new_restaurant.service()
 
 my_restaurant = Restaurant('Noodles & Co', 'Fusion Pasta Meals')
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
+my_restaurant.increment_number_served(100)
+my_restaurant.service()
 
 her_restaurant = Restaurant('Panda Express', 'Asian Food')
 her_restaurant.describe_restaurant()
 her_restaurant.open_restaurant()
-
+her_restaurant.increment_number_served(300)
+her_restaurant.service()
